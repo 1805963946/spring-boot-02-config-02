d28569783692a61c6cf82b2065afe0e808958db5
Zhu <1805963946@qq.com>
2020-09-02 18:48:36 +0800

    删除

--- src/main/java/com/atguigu/springboot02config02/mybatais/test.java
+++
@@ -1,4 +0,0 @@
-package com.atguigu.springboot02config02.mybatais;
-
-public interface test {
-}
--- src/test/java/test2/Person.java
+++
@@ -1,6 +0,0 @@
-package test2;
-
-public class Person {
-    protected String name;
-    public String name1;
-}
